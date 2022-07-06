b4a30c63b599968ff81b790df997bd27a71cfa98
markcaf <marcocafassog@gmail.com>
2022-07-06 13:04:03 +0200

    Added to-do list items in instance data

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ const app = new Vue(
         el: '#app',
 
         data: {
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                },
+            ]
             
         },
 
